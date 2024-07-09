e77f2836409b4b13aa7e38337c789d50a9bafe21
Marc <marc@highcard.dev>
2024-07-09 21:32:08 +0200

    chore: wget command

--- .docker/druid-install-command.sh
+++ .docker/druid-install-command.sh
@@ -1,7 +1,7 @@
-curl -L -o /app/resources/druid https://github.com/highcard-dev/druid-cli/releases/latest/download/druid
-curl -L -o /app/resources/druid_rcon https://github.com/highcard-dev/druid-cli/releases/latest/download/druid_rcon 
-curl -L -o /app/resources/druid_rcon_web https://github.com/highcard-dev/druid-cli/releases/latest/download/druid_rcon_web
-curl -L -o /app/resources/entrypoint.sh https://github.com/highcard-dev/druid-cli/releases/latest/download/entrypoint.sh 
+wget -O /app/resources/druid https://github.com/highcard-dev/druid-cli/releases/latest/download/druid
+wget -O /app/resources/druid_rcon https://github.com/highcard-dev/druid-cli/releases/latest/download/druid_rcon 
+wget -O /app/resources/druid_rcon_web https://github.com/highcard-dev/druid-cli/releases/latest/download/druid_rcon_web
+wget -O /app/resources/entrypoint.sh https://github.com/highcard-dev/druid-cli/releases/latest/download/entrypoint.sh 
 chmod +x /app/resources/druid /app/resources/druid_rcon /app/resources/druid_rcon_web
 
 export PATH=$PATH:/app/resources
